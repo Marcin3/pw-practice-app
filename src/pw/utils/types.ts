980f2f2d1040b20a296c271d963da8fc34607824
Marcin Szafranek <marcin.szafranek@gmail.com>
2025-04-02 16:37:52 +0200

    form-layout-page

--- src/pw/utils/types.ts
+++ src/pw/utils/types.ts
@@ -1,6 +1,20 @@
+import { z } from 'zod';
+
+const EmailSchema = z.string().email();
+type Email = z.infer<typeof EmailSchema>;
+
 export interface SliderPageFields {
   valueLocatorCss: string;
   draggerIndex: number;
   sliderIndex: number;
   errorMessageField:string
 }
+
+export  interface User {
+  firstName: string;
+  lastName: string;
+  email: Email;
+  website: string;
+}
+
+export type BasicUser =  Pick<User, 'firstName' | 'lastName'>
